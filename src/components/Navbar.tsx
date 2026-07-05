fe276c39dd1bf914b043522b1cd1d7ecbf1331c9
metrocardealsng-svg <metrocardealsng@gmail.com>
2026-07-05 19:07:37 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,6 +42,7 @@ export default function Navbar() {
     { href: "/pricing", label: "Pricing" },
    
     { href: "/tribe", label: "Tribe" },
+    { href: "/profile", label: "Profile" },
     { href: "/compare", label: "Compare" },
   
     { href: "/best/step-tracking-apps", label: "Best Apps" },
